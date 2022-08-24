ef88c6832a6b32e4d85c460a416257a44bbed04d
bhatiaji112211 <102137520+bhatiaji112211@users.noreply.github.com>
2022-08-24 15:29:22 +0530

    casual update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 $("button").click(function(){
     $("ul").slideToggle();
 });
+
+function animate_string(id){
+    const element = document.getElementById(id);
+    const textNode = element.childNodes[0];
+    let text = textNode.data;
+
+    setInterval(()=>{
+        text = text[text.length - 1] + text.substring(0, text.length -1);
+        textNode.data = text;
+    }, 200);
+}
